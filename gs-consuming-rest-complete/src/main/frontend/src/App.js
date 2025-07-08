133c9d41aefdaddc50294135aa285ead4678c850
kimchanbae <kimchanbae@DESKTOP-PLRLBEJ>
2025-07-08 11:49:31 +0900

    git 연동 commit test

---
+++ gs-consuming-rest-complete/src/main/frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Hello from './Hello';
+import Sample from './Sample';
+
+function App() {
+	/*const [hello, setHello] = useState('');
+  	const [error, setError] = useState('');*/
+  	
+	/*const [data, setData] = useState([]);*/
+	
+	/*useEffect(() => {
+    	axios.get('/hello')
+        .then((res) => {
+          	setData(res.data);
+        })
+        .catch((err) => {
+          	setError(err.message);
+        });
+  	}, []);*/
+	
+	/*useEffect(() => {
+	   	fetch("/hello")
+       	.then((res) => {
+         	return res.json();
+       	})
+       	.then(function (result) {
+           	setData(result);
+     	})
+	 },[]); */ 
+	
+	return (
+    /*<div className="App">
+		백엔드에서 받은 데이터: {hello}
+		{error && <p>Error: {error}</p>}
+    </div>*/
+	
+	<div>
+      	<BrowserRouter>
+          	<Routes>
+		  		<Route path="/" element={<Hello />} />
+				<Route path="/hello" element={<Hello />} />  
+            	<Route path="/sample" element={<Sample/>}/>
+        	</Routes>
+      	</BrowserRouter>
+	</div>
+  );
+}
+
+export default App;
